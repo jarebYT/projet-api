6897dc78f1cec11eb5b83d025a7f7104d2f9a3f8
lxquaver <bernard.luca1206@gmail.com>
2025-09-25 11:45:29 +0200

    [+] add commentaries to controllers

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,18 +1,19 @@
-const User = require('./../model/user.model');
-const jwt = require('jsonwebtoken');
-const bcryptjs = require('bcryptjs');
-require('dotenv').config();
+    const User = require('./../model/user.model');
+    const jwt = require('jsonwebtoken');
+    const bcryptjs = require('bcryptjs');
+    require('dotenv').config();
 
-exports.getById = async (req,res) => {
-    try{
-        let user = await User.findOne({
-            where: {
-                id: req.params.id
-            }
-        });
-        res.status(200).json({id: user.id, username: user.username, email: user.email});
-    }catch(e){
-        res.status(400).json({error: "Impossible de récupérer les utilsateurs"})
+    // Récupère un utilisateur par identifiant.
+    exports.getById = async (req,res) => {
+        try{
+            let user = await User.findOne({
+                where: {
+                    id: req.params.id
+                }
+            });
+            res.status(200).json({id: user.id, username: user.username, email: user.email});
+        }catch(e){
+            res.status(404).json({error: "Impossible de récupérer les utilsateurs"})
+        }
     }
-}
 
