c85a85a94bc0e91e50028ad8b6e7484d0523d87c
chami212 <hicham.akarkach@outlook.fr>
2025-09-25 10:30:22 +0200

    [+] main: Created auth.controller

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userRoute = require('./route/user.route');
+const authRoute = require("./route/auth.route");
 const commentRoute = require('./route/comment.route');
 const postRoute = require('./route/post.route');
 const path = require('path');
@@ -11,7 +12,8 @@ app.use(express.json());
 
 app.use('/images', express.static(path.join(__dirname, "images")));
 
-app.use('/auth', userRoute);
+app.use("/users", userRoute);
+app.use('/auth', authRoute);
 app.use('/comments', commentRoute);
 app.use('/posts', postRoute);
 
