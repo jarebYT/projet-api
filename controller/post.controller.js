cba391c310cc10e53580653c457cd9ef9da61e7d
jarebYT <jarebYT@hotmail.com>
2025-09-25 10:09:35 +0200

    [+] main: CHanged the sync.js file

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -1,4 +1,4 @@
-const Post = require('./../model/Post.model');
+const Post = require('./../model/post.model');
 
 
 exports.getAll = async (req, res) => {
--- model/sync.js
+++ model/sync.js
@@ -1,9 +1,18 @@
-const bdd = require('./index');
+// sync.js
+const db = require("./index");
+
+// Importer les modèles pour qu'ils soient enregistrés dans Sequelize
+require("./post.model");
+require("./user.model");   // si tu as d'autres modèles
+require("./comment.model"); // idem
 
 const sync = async () => {
-    await relate();
-    await bdd.sync({alter: true});
-    console.log("Synchronisation réussie");
-}
+  try {
+    await db.sync({ alter: true }); // crée ou met à jour les tables
+    console.log("✅ Synchronisation réussie");
+  } catch (err) {
+    console.error("❌ Erreur de synchronisation :", err);
+  }
+};
 
 sync();
